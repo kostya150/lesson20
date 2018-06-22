9b75623305c5429e08671344914fbc97db64bc18
kostya150 <kostyazhivotov@gmail.com>
2018-06-22 13:02:11 +0300

    lesson20 review 5

--- task2/Controller.java
+++ task2/Controller.java
@@ -5,6 +5,8 @@ import lesson20.task2.exception.InternalServerException;
 
 public class Controller {
     private TransactionDAO transactionDAO = new TransactionDAO();
+    private Transaction[] transactions = new Transaction[10];
+
 
     public Transaction save(Transaction transaction) throws BadRequestException, InternalServerException {
         return transactionDAO.save(transaction);
@@ -14,11 +16,17 @@ public class Controller {
         return transactionDAO.transactionList();
 
     }
-    public Transaction[] transactionList(String city) throws BadRequestException {
-        return transactionDAO.transactionList(Transaction.getCity());
+    public Transaction[] transactionList(String city) throws InternalServerException, BadRequestException {
+        for (int i = 0; i < transactions.length; i++){
+        return transactionDAO.transactionList(transactions[i].getCity());
+        }
+        throw new InternalServerException("Error");
 
     }
-    public Transaction[] transactionList(int amount) throws BadRequestException {
-        return transactionDAO.transactionList(Transaction.getAmount());
+    public Transaction[] transactionList(int amount) throws BadRequestException, InternalServerException {
+        for (int i = 0; i < transactions.length; i++){
+        return transactionDAO.transactionList(transactions[i].getAmount());
+        }
+        throw new InternalServerException("Error");
     }
 }
